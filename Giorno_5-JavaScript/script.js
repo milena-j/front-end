5078eded1e63a684ed11c660a1c65240a9d6d58d
milena-j <milenajocic.work@gmail.com>
2023-10-30 18:01:00 +0100

    altro

---
+++ Giorno_5-JavaScript/script.js
@@ -0,0 +1,5 @@
+console.log('espressione'); //stampa in console il contenuto delle parentesi tonde
+
+//ogni volta che ci sono le parentesi tonde si tratta di una funzione
+
+prompt() //funz di base che ammette come parametri opzionali un messaggio e un valore di default, chiede di inserire un valore tramite una finestra e restituisce una stringa -quando l'utente clicca su ok- o un null -quando l'utente clicca su annulla-
