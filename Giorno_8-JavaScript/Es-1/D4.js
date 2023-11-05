fbec80a5f7777baeefdcbc342caaea8cf598f759
milena-j <milenajocic.work@gmail.com>
2023-11-05 23:48:50 +0100

    es js

--- Giorno_8-JavaScript/Es-1/D4.js
+++ Giorno_8-JavaScript/Es-1/D4.js
@@ -1,68 +1,251 @@
 /* ESERCIZIO 1
- Scrivi una funzione di nome "area", che riceve due parametri (l1, l2) e calcola l'area del rettangolo associato..
+ Scrivi una funzione di nome "area", che riceve due 
+ parametri (l1, l2) e calcola l'area del rettangolo 
+ associato..
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+function area(l1, l2) {
+    let a = l1 * l2;
+    return a;
+}
+
+let a = area(3, 9);
+console.log(a)
 
 /* ESERCIZIO 2
- Scrivi una funzione di nome "crazySum", che riceve due numeri interi come parametri.
- La funzione deve ritornare la somma dei due parametri, ma se il valore dei due parametri è il medesimo deve invece tornare
- la loro somma moltiplicata per tre.
+ Scrivi una funzione di nome "crazySum", che riceve 
+ due numeri interi come parametri.
+ La funzione deve ritornare la somma dei due parametri, 
+ ma se il valore dei due parametri è il medesimo deve 
+ invece tornare la loro somma moltiplicata per tre.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+function crazySum(x, y) {
+    if (x === y) {
+        return (x + y) * 3;
+    } else {
+        return x + y;
+    }
+}
+
+console.log(crazySum(5, 10))
+
+//oppure
+
+function crazySum2(x, y) {
+    if (x !== y) {
+        return x + y;
+    } else {
+        return (x + y) * 3;
+    }
+}
+
+console.log(crazySum2(10, 10))
+
 
 /* ESERCIZIO 3
- Scrivi una funzione di nome "crazyDiff" che calcola la differenza assoluta tra un numero fornito come parametro e 19.
- Deve inoltre tornare la differenza assoluta moltiplicata per tre qualora il numero fornito sia maggiore di 19.
+ Scrivi una funzione di nome "crazyDiff" che calcola 
+ la differenza assoluta tra un numero fornito come 
+ parametro e 19.
+ Deve inoltre tornare la differenza assoluta 
+ moltiplicata per tre qualora il numero fornito sia 
+ maggiore di 19.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+function crazyDiff(z) {
+    if (z > 19) {
+        return Math.abs(z - 19) * 3;
+    } else {
+        return Math.abs(z - 19);
+    }
+}
+
+console.log(crazyDiff(10))
+
+//oppure
+
+function crazyDiff1(z) {
+
+    let res = Math.abs(z - 19)
+
+    if (z > 19) {
+        return res * 3;
+    }
+    return res;
+}
+
+console.log(crazyDiff1(30))
+
 /* ESERCIZIO 4
- Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure
+ Scrivi una funzione di nome "boundary" che accetta 
+ un numero intero n come parametro, e ritorna true 
+ se n è compreso tra 20 e 100 (incluso) oppure
  se n è uguale a 400.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+function boundary(n) {
+    if (n > 20 && n <= 100 || n === 400) {
+        return true;
+    }
+    return false;
+}
+
+console.log(boundary(50))
+
 /* ESERCIZIO 5
- Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
- La funzione deve aggiungere la parola "EPICODE" all'inizio della stringa fornita, ma se la stringa fornita comincia già con "EPICODE" allora deve
+ Scrivi una funzione di nome "epify" che accetta 
+ una stringa come parametro.
+ La funzione deve aggiungere la parola "EPICODE" 
+ all'inizio della stringa fornita, ma se la stringa 
+ fornita comincia già con "EPICODE" allora deve
  ritornare la stringa originale senza alterarla.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+function epify(str) {
+    if (str.startsWith("EPICODE")) {
+        return str;
+    } else {
+        return "EPICODE" + str;
+    }
+}
+
+console.log(epify(" non è contenuto nella stringa"))
+
 /* ESERCIZIO 6
- Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
- di 3 o di 7. (Suggerimento: usa l'operatore modulo)
+ Scrivi una funzione di nome "check3and7" che 
+ accetta un numero positivo come parametro. 
+ La funzione deve controllare che il parametro 
+ sia un multiplo di 3 o di 7. 
+ (Suggerimento: usa l'operatore modulo)
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+function check3and7(num) {
+    if (num <= 0) {
+        return "errore: il numero è negativo"
+    } else if (num % 3 === 0) {
+        return "il numero è multiplo di 3";
+    } else if (num % 7 === 0) {
+        return "il numero è multiplo di 7";
+    } else {
+        return "il numero non è multiplo di 3 o 7"
+    }
+}
+
+console.log(check3and7(49))
+
+//oppure
+
+function check3e7(num) {
+    if (num > 0) {
+        if (num % 3 === 0) {
+            return "il numero è multiplo di 3";
+        } if (num % 7 === 0) {
+            return "il numero è multiplo di 7";
+        }
+        return "il numero non è multiplo di 3 o 7"
+    }
+    return "errore: il numero è negativo"
+}
+
+console.log(check3e7(53))
 
 /* ESERCIZIO 7
- Scrivi una funzione di nome "reverseString", il cui scopo è invertire una stringa fornita come parametro (es. "EPICODE" --> "EDOCIPE")
+ Scrivi una funzione di nome "reverseString", 
+ il cui scopo è invertire una stringa fornita 
+ come parametro (es. "EPICODE" --> "EDOCIPE")
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+function reverseString(string) {
+    return string.split("").reverse().join("");
+}
+
+console.log(reverseString("EPICODE"))
 
 /* ESERCIZIO 8
- Scrivi una funzione di nome "upperFirst", che riceve come parametro una stringa formata da diverse parole.
- La funzione deve rendere maiuscola la prima lettera di ogni parola contenuta nella stringa.
+ Scrivi una funzione di nome "upperFirst", 
+ che riceve come parametro una stringa formata 
+ da diverse parole.
+ La funzione deve rendere maiuscola la prima 
+ lettera di ogni parola contenuta nella stringa.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+function upperFirst(frase) {
+    
+    let words = frase.split(" ");  //creo un array in cui inserire i singoli elementi della stringa
+
+    for (let i = 0; i < words.length; i++) {     // itero ogni elemento dell'array
+        let splittata = words[i].split("");     // inserisco in una variabile i singoli caratteri della parola
+        splittata[0] = splittata[0].toUpperCase(); // trasformo il primo carattere in maiuscolo e lo inserisco all'inice 0 della var
+        words[i] = splittata.join("");          // riunisco i caratteri in una parola unica, salvandola nell'array words
+    }
+    return words.join(" ");
+}
+
+console.log(upperFirst("stringa formata da diverse parole"))
+
+// FOR EACH
+
+function upperFirst(frase) {
+    
+    let words = frase.split(" ");  //creo un array in cui inserire i singoli elementi della stringa
+
+    for(let i in words){
+        let splittata = words[i].split("");     
+        splittata[0] = splittata[0].toUpperCase(); 
+        words[i] = splittata.join("");
+    }
+    return words.join(" ");
+}
+
+console.log(upperFirst("una stringa formata da diverse parole"))
+
 /* ESERCIZIO 9
- Scrivi una funzione di nome "cutString", che riceve come parametro una stringa. La funzione deve creare una nuova stringa senza il primo e l'ultimo carattere
+ Scrivi una funzione di nome "cutString", 
+ che riceve come parametro una stringa. 
+ La funzione deve creare una nuova stringa 
+ senza il primo e l'ultimo carattere
  della stringa originale.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+function cutString(stringa) {
+    return stringa.slice(1, stringa.length -1);
+}
+
+console.log(cutString("una stringa formata da diverse parole"))
+
 /* ESERCIZIO 10
- Scrivi una funzione di nome "giveMeRandom", che accetta come parametro un numero n e ritorna un'array contenente n numeri casuali inclusi tra 0 e 10.
+ Scrivi una funzione di nome "giveMeRandom", 
+ che accetta come parametro un numero n e 
+ ritorna un'array contenente n numeri casuali 
+ inclusi tra 0 e 10.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+function giveMeRandom(n) {
+    let arr = [];
+    for(let i=0; i<n; i++){
+        arr.push(Math.round(Math.random() * 10));
+    }
+    return arr
+}
+
+console.log(giveMeRandom(5))
--- Giorno_8-JavaScript/Es-2-extra/D4-EXTRA.js
+++ Giorno_8-JavaScript/Es-2-extra/D4-EXTRA.js
@@ -1,79 +1,289 @@
 // Esercizi aggiuntivi (facoltativi) per D4
 
 /* EXTRA 1
- Scrivi una funzione chiamata "checkArray" che riceve un array di numeri casuali (creati con la funzione "giveMeRandom") e per ogni elemento stampa in console
+ Scrivi una funzione chiamata "checkArray" che riceve un array di numeri casuali 
+ (creati con la funzione "giveMeRandom") e per ogni elemento stampa in console
  se il suo valore è maggiore di 5 o no.
  La funzione deve inoltre ritornare la somma di tutti i valori maggiori di 5.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+function giveMeRandom(n) {
+    let arr = [];
+    for (let i = 0; i < n; i++) {
+        arr.push(Math.round(Math.random() * 10));
+    }
+    return arr
+}
+
+function checkArray() {
+    let numRan = giveMeRandom(10);
+    console.log(numRan);
+    let somma = 0;
+    let x = [];
+
+    for (let i in numRan) {
+        if (numRan[i] > 5) {
+            x.push(true);
+            somma += numRan[i];
+        } else {
+            x.push(false);
+        }
+    } console.log(x);
+    return somma;
+}
+
+console.log(checkArray())
+
 /* EXTRA 2
- Nel tuo eCommerce disponi di un'array di oggetti chiamato "shoppingCart". Ognuno di questi oggetti ha le seguenti proprietà: "price", "name", "id" e "quantity".
- Crea una funzione chiamata "shoppingCartTotal" che calcola il totale dovuto al negozio (tenendo conto delle quantità di ogni oggetto).
+ Nel tuo eCommerce disponi di un'array di oggetti chiamato "shoppingCart". Ognuno di questi oggetti 
+ ha le seguenti proprietà: "price", "name", "id" e "quantity".
+ Crea una funzione chiamata "shoppingCartTotal" che calcola il totale dovuto al negozio (tenendo conto 
+delle quantità di ogni oggetto).
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+let shoppingCart = [
+    {
+        price: 27,
+        name: "prodA",
+        id: 102030,
+        quantity: 2
+    },
+    {
+        price: 93,
+        name: "prodB",
+        id: 405060,
+        quantity: 1
+    }
+];
+
+function shoppingCartTotal() {
+    let y = 0;
+
+    for (let i in shoppingCart) {
+        y += shoppingCart[i].price * shoppingCart[i].quantity;
+    } return y;
+}
+
+console.log(shoppingCartTotal())
 
 /* EXTRA 3
- Nel tuo eCommerce disponi di un'array di oggetti chiamato "shoppingCart". Ognuno di questi oggetti ha le seguenti proprietà: "price", "name", "id" e "quantity".
- Crea una funzione chiamata "addToShoppingCart" che riceve un nuovo oggetto dello stesso tipo, lo aggiunge a "shoppingCart" e ritorna il nuovo numero totale degli elementi.
+ Nel tuo eCommerce disponi di un'array di oggetti 
+ chiamato "shoppingCart". Ognuno di questi oggetti 
+ ha le seguenti proprietà: "price", "name", "id" 
+ e "quantity".
+ Crea una funzione chiamata "addToShoppingCart" 
+ che riceve un nuovo oggetto dello stesso tipo, 
+ lo aggiunge a "shoppingCart" e ritorna il nuovo 
+ numero totale degli elementi.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+function addToShoppingCart() {
+
+    let newProd = {
+        price: 37,
+        name: "prodC",
+        id: 708090,
+        quantity: 4
+    }
+    shoppingCart.push(newProd);
+    return shoppingCart;
+}
+
+console.log(addToShoppingCart())
+console.log(shoppingCartTotal())
 
 /* EXTRA 4
- Nel tuo eCommerce disponi di un'array di oggetti chiamato "shoppingCart". Ognuno di questi oggetti ha le seguenti proprietà: "price", "name", "id" e "quantity".
- Crea una funzione chiamata "maxShoppingCart" che riceve l'array "shoppingCart" e ritorna l'oggetto più costoso in esso contenuto.
+ Nel tuo eCommerce disponi di un'array di oggetti 
+ chiamato "shoppingCart". Ognuno di questi oggetti 
+ ha le seguenti proprietà: "price", "name", "id" 
+ e "quantity".
+ Crea una funzione chiamata "maxShoppingCart" che 
+ riceve l'array "shoppingCart" e ritorna l'oggetto 
+ più costoso in esso contenuto.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+function maxShoppingCart() {
+    let indice = 0;
+    let prezzoMax = 0;
+    for (let i in shoppingCart) {
+        if (shoppingCart[i].price > prezzoMax) {
+            prezzoMax = shoppingCart[i].price
+            indice = i;
+        }
+    } return shoppingCart[indice];
+}
+
+console.log(maxShoppingCart())
+
 
 /* EXTRA 5
- Nel tuo eCommerce disponi di un'array di oggetti chiamato "shoppingCart". Ognuno di questi oggetti ha le seguenti proprietà: "price", "name", "id" e "quantity".
- Crea una funzione chiamata "latestShoppingCart" che riceve l'array "shoppingCart" e ritorna l'ultimo elemento.
+ Nel tuo eCommerce disponi di un'array di oggetti 
+ chiamato "shoppingCart". Ognuno di questi oggetti 
+ ha le seguenti proprietà: "price", "name", "id" 
+ e "quantity".
+ Crea una funzione chiamata "latestShoppingCart" 
+ che riceve l'array "shoppingCart" e ritorna l'ultimo 
+ elemento.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+function latestShoppingCart() {
+    return shoppingCart[shoppingCart.length - 1]
+}
+
+console.log(latestShoppingCart())
 
 /* EXTRA 6
- Crea una funzione chiamata "loopUntil" che riceve un numero intero come parametro con valore tra 0 e 9.
- La funzione è composta da un ciclo che stampa un numero casuale tra 0 e 9 finchè il numero casuale non è maggiore di x per tre volte di fila.
+ Crea una funzione chiamata "loopUntil" che riceve 
+ un numero intero come parametro con valore tra 0 e 9.
+ La funzione è composta da un ciclo che stampa 
+ un numero casuale tra 0 e 9 finchè il numero 
+ casuale non è maggiore di x per tre volte di fila.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+function loopUntil(y) {
+    let count = 0;
+
+    while (count < 3) {
+        let z = Math.random() * 9;
+        console.log(z)
+        if (z > y) {
+            count++
+        } else {
+            count = 0;
+        }
+    }
+}
+
+console.log(loopUntil(5))
 
 /* EXTRA 7
-Crea una funzione chiamata "average" che riceve un array come parametro e ne ritorna la media aritmetica. La funzione salta automaticamente i valori non numerici nell'array.
+Crea una funzione chiamata "average" che riceve 
+un array come parametro e ne ritorna la media 
+aritmetica. La funzione salta automaticamente 
+i valori non numerici nell'array.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+function average(array) {
+    let b = 0;
+    let c = 0;
+
+    for (let i in array) {
+        if (typeof array[i] === 'number') {
+            b += array[i];
+            c++;
+        }
+    } return b / c
+}
+
+console.log(average([2, 7, 4, "ciao", 5, "x", 9]))
 
 /* EXTRA 8
- Crea una funzione chiamata "longest" che trova la stringa più lunga all'interno di un array di stringhe fornito come parametro.
+ Crea una funzione chiamata "longest" che trova 
+ la stringa più lunga all'interno di un array 
+ di stringhe fornito come parametro.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+function longest(array) {
+    let valMax = 0;
+    let indice = 0;
+
+    for (let i in array) {
+        let c = array[i].length;
+        if (c > valMax) {
+            valMax = c;
+            indice = i;
+        }
+    }
+    return array[indice]
+}
+
+console.log(longest(["prima", "seconda", "terza", "quarta"]))
 
 /* EXTRA 9
- Crea una funzione per creare un filtro anti-spam per la tua casella email. La funzione riceve un parametro stringa chiamato "emailContent", e torna un valore booleano.
- La funzione deve ritornare true se "emailContent" non contiene le parole "SPAM" o "SCAM".
+ Crea una funzione per creare un filtro anti-spam 
+ per la tua casella email. La funzione riceve un 
+ parametro stringa chiamato "emailContent", 
+ e torna un valore booleano.
+ La funzione deve ritornare true se "emailContent" 
+ non contiene le parole "SPAM" o "SCAM".
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+function filtro(emailContent) {
+    return (emailContent.indexOf("SPAM") > 0 || emailContent.indexOf("SCAM") > 0)
+}
+
+console.log(filtro("CIAO SPAM"))
+console.log(filtro("CIAO"))
+console.log(filtro("CIAO SCAM"))
+console.log(filtro("SPAM SCAM"))
 
 /* EXTRA 10
- Scrivi una funzione che riceve una data come parametro, e calcola il numero di giorni passati da quella data.
+ Scrivi una funzione che riceve una data come 
+ parametro, e calcola il numero di giorni passati 
+ da quella data.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+function calcoloData(data) {
+    // Converte la data passata come parametro in un oggetto Data.
+    const dataFornita = new Date(data);
+
+    // Ottiene la data corrente.
+    const dataCorrente = new Date();
+
+    // Calcola la differenza tra le due date in millisecondi.
+    const differenzaInMillisecondi = dataCorrente - dataFornita;
+
+    // Converte la differenza in millisecondi in giorni.
+    const millisecondiInUnGiorno = 1000 * 60 * 60 * 24;
+    const giorniPassati = Math.floor(differenzaInMillisecondi / millisecondiInUnGiorno);
+
+    return giorniPassati;
+}
+
+console.log(calcoloData("2022-11-05"))
+
 /* EXTRA 11
- Scrivi una funzione chiamata "matrixGenerator" che riceve come parametri due numeri interi, "x" e "y".
- Il risultato deve essere una matrice di "x" volte "y", e i valori devono rispecchiare gli indici della posizione all'interno della matrice.
+ Scrivi una funzione chiamata "matrixGenerator" 
+che riceve come parametri due numeri interi, 
+ "x" e "y".
+ Il risultato deve essere una matrice di
+ "x" volte "y", e i valori devono rispecchiare 
+ gli indici della posizione all'interno della matrice.
  Es.: x = 3, y = 2
  ["00","01","02"
  "10","11","12"]
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+function matrixGenerator(x, y) {
+    let a = [];
+    for (let i = 0; i < x; i++) {
+        let row = [];
+        for (let j = 0; j < y; j++) {
+            row.push(i + '' + j);
+        } a.push(row);
+    } return a;
+}
+
+console.log(matrixGenerator(7, 3))
